b05d5edcc2d5d144970bdfd1b34e9ba2bf3dbe70
PaulArias27 <paularias2727@gmail.com>
2026-05-25 00:26:01 -0500

    PA:se inserto datos a la tabla

--- tabla_estudiantes.sql
+++ tabla_estudiantes.sql
@@ -10,3 +10,19 @@ create table estudiantes(
 	constraint estudiantes_pk primary key(id_estudiante)
 	
 )
+--insertar datos
+INSERT INTO  estudiantes VALUES(1,'Juan','Perez',20,'Programcion','2026-01-10');
+INSERT INTO  estudiantes VALUES(2,'Paul','Arias',27,'software','2026-01-04');
+INSERT INTO  estudiantes VALUES(3,'Ana','Verde',24,'terapia','2026-02-11');
+INSERT INTO  estudiantes VALUES(4,'Pedro','Vite',28,'ia','2026-03-12');
+INSERT INTO  estudiantes VALUES(5,'Jean','Perez',40,'cajero','2026-04-13');
+INSERT INTO  estudiantes VALUES(6,'Meche','Fajardo',59,'recursos','2026-05-14');
+INSERT INTO  estudiantes VALUES(7,'Ninoska','Zambrano',19,'medicina','2026-02-15');
+INSERT INTO  estudiantes VALUES(8,'Gabriela','Peralta',20,'enfermeria','2026-01-16');
+INSERT INTO  estudiantes VALUES(9,'Andrius','Burgos',23,'criminalistica','2026-01-17');
+INSERT INTO  estudiantes VALUES(10,'Andres','Lopez',18,'banco','2026-04-18');
+INSERT INTO  estudiantes VALUES(11,'Gabriela','Rivera',23,'leguas','2026-05-19');
+INSERT INTO  estudiantes VALUES(12,'Nicolas','Rivera',16,'ciberseguridad','2026-02-20');
+INSERT INTO  estudiantes VALUES(13,'Valeria','Rivera',24,'terapia','2026-01-21');
+INSERT INTO  estudiantes VALUES(14,'Rosita','Martillo',30,'psicologia','2026-03-22');
+INSERT INTO  estudiantes VALUES(15,'Cristina','Martines',35,'enfermeria','2026-02-23');
